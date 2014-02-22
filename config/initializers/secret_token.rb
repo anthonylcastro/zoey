4c4f89bf6a1edb1129d0a2794c48f72a656f8c8e
Anthony Castro <anthonylcastro@gmail.com>
2014-02-22 10:37:03 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstBlog2::Application.config.secret_key_base = '4c08cab55a22f9296dcd75b917526dcb4de31c68e52b3a3aada0f50336220ea076af8dc78ac4a820124f04970c04d56a1f241e6266f0a8a85c0f7e42eaab6273'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstBlog2::Application.config.session_store :cookie_store, key: '_FirstBlog2_session'
